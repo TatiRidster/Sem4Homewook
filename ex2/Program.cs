878426771e5e09ebd7157f939315ce9f9542e61e
Tatiana <tatnovid@gmail.com>
2022-05-24 16:49:44 +0300

    задача про сумму цифр в числе

---
+++ ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и
+// выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+int SumOfNumber(int num)
+  {
+    int res = 0;
+    int sum = 0;
+    int count = Convert.ToString(num).Length;
+    for (int i = 0; i < count; i++)
+    {
+      res = num - num % 10;
+      sum = sum + (num - res);
+      num = num / 10;
+    }
+   return sum;
+  }
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sumofnumber = SumOfNumber(num);
+Console.WriteLine($"Сумма цифр в числе: {sumofnumber} ");
